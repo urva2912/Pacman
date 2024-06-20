3bca1b295b3c52420ceebf1c57c6b7180d9b9c29
Urvashi Pansuriya <urvapansuriya2912@gmail.com>
2024-06-20 14:58:01 +1200

    movement of pacman and ghoul

--- PacmanGame/PacmanGame/Ghoul.cs
+++ PacmanGame/PacmanGame/Ghoul.cs
@@ -1,13 +1,66 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace PacmanGame
 {
     public class Ghoul : Creature
     {
-        public Ghoul(int x, int y, Image image) : base(x, y, image) 
+        private Random random;
+
+        public Ghoul(int x, int y, Image image) : base(x, y, image)
         {
+            random = new Random();  // Initialize random number generator
         }
 
-        // Custom behavior for Ghouls can be added here
+        public void MoveTowardsPacman(Pacman pacman)
+        {
+            int deltaX = pacman.X - this.X;
+            int deltaY = pacman.Y - this.Y;
+            int stepSize = 5; // Increase step size to 10 pixels per move
+
+            // Adjust Ghoul's position based on direction towards Pacman
+            if (Math.Abs(deltaX) > Math.Abs(deltaY))
+            {
+                if (deltaX > 0 && X + stepSize < 800 - Image.Width) // Check right boundary
+                    X += stepSize; // Move right
+                else if (deltaX < 0 && X - stepSize > 0) // Check left boundary
+                    X -= stepSize; // Move left
+            }
+            else
+            {
+                if (deltaY > 0 && Y + stepSize < 600 - Image.Height) // Check bottom boundary
+                    Y += stepSize; // Move down
+                else if (deltaY < 0 && Y - stepSize > 0) // Check top boundary
+                    Y -= stepSize; // Move up
+            }
+        }
+
+        public void MoveRandomly()
+        {
+            int direction = random.Next(4);  // 0: up, 1: down, 2: left, 3: right
+            int stepSize = 5; // Increase step size to 5 pixels per move
+
+            switch (direction)
+            {
+                case 0:  // Up
+                    if (Y - stepSize > 0) // Check top boundary
+                        Y -= stepSize;
+                    break;
+                case 1:  // Down
+                    if (Y + stepSize < 600 - Image.Height) // Check bottom boundary
+                        Y += stepSize;
+                    break;
+                case 2:  // Left
+                    if (X - stepSize > 0) // Check left boundary
+                        X -= stepSize;
+                    break;
+                case 3:  // Right
+                    if (X + stepSize < 800 - Image.Width) // Check right boundary
+                        X += stepSize;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
